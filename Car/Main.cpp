2129d1d759f1bf11af4fb5eb0e2c5ce5b9769532
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-04-24 11:35:33 -0400

    Car in progress

--- Car/Main.cpp
+++ Car/Main.cpp
@@ -2,11 +2,14 @@
 #include<iostream>
 #include<conio.h>
 #include<thread>
+#include<atomic>
+#include<chrono>
 using namespace std;
 
 #define Escape 27
 #define Enter 13
 #define Space 32
+
 
 #define MIN_TANK_CAPACITY 20
 #define MAX_TANK_CAPACITY 120
@@ -17,9 +20,9 @@ using namespace std;
 
 class Tank
 {
+	const int CAPACITY;
 	double fuel_level;
 public:
-	const int CAPACITY;
 	double get_fuel_level()const
 	{
 		return fuel_level;
@@ -30,7 +33,7 @@ public:
 		capacity > MAX_TANK_CAPACITY ? MAX_TANK_CAPACITY :
 		capacity
 	),
-		fuel_level(0)
+		fuel_level(CAPACITY * 0.5)
 	{
 		cout << "Tank:";
 		if (capacity < MIN_TANK_CAPACITY)cout << "Min capacity was applied" << endl;
@@ -53,6 +56,21 @@ public:
 			cout << "Tank is full" << endl;
 		}
 		return fuel_level;
+	}
+	double give_fuel(double amount)
+	{
+		if (amount < 0)return 0;
+		if (fuel_level >= amount)
+		{
+			fuel_level -= amount;
+			return amount;
+		}
+		else
+		{
+			double remaining = fuel_level;
+			fuel_level = 0;
+			return remaining;
+		}
 	}
 	void info()const
 	{
@@ -115,9 +133,12 @@ class Car
 {
 	Engine engine;
 	Tank tank;
+	bool engine_running;
+	bool driver_inside;
+	bool panel_active;
 	int speed;
 	const int MAX_SPEED;
-	bool driver_inside;
+
 public:
 	Car(double consumption, int capacity, int max_speed = 250) :
 		MAX_SPEED
@@ -128,7 +149,10 @@ public:
 		),
 		engine(consumption),
 		tank(capacity),
-		speed(0)
+		speed(0),
+		engine_running(false),
+		driver_inside(false),
+		panel_active(false)
 	{
 		driver_inside = false;
 		cout << "Your car is ready to go, press 'Enter' to get in ;-)" << endl;
@@ -137,23 +161,43 @@ public:
 	{
 		cout << "The car is over" << endl;
 	}
-	/*void start_engine()
+	void engine_working() 
 	{
-		if (tank.get_fuel_level() > 0)
+		while (engine_running) 
 		{
-			engine.start();
-			thread([this]() {this->}
-		}
-	}*/
-	void engine_working()
-	{
-		while (engine.started() && tank.get_fuel_level() > 0)
-		{
+			if (tank.get_fuel_level() <= 0) 
+			{
+				engine_running = false;
+				break;
+			}
 			double consumption = engine.get_consumption_per_second();
-			
+			tank.give_fuel(consumption);
+			this_thread::sleep_for(chrono::seconds(1));
 		}
 	}
-	void get_in()
+	void panel_working() 
+	{
+		while (panel_active) 
+		{
+			system("cls");
+			cout << "Fuel level: " << tank.get_fuel_level() << " liters\n";
+			cout << "Engine status: " << (engine_running ? "RUNNING" : "STOPPED") << "\n";
+			cout << "Consumption: " << engine.get_consumption_per_second() << " liters/s\n";
+			cout << "Speed: " << speed << " km/h\n";
+			cout << "Press SPACE to stop engine\n";
+			cout << "Press ENTER to exit car\n";
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+	}
+	void info()const
+	{
+			tank.info();
+			engine.info();
+			cout << "Speed: " << speed << " km/h\n";
+			cout << "MaxSpeed: " << MAX_SPEED << " km/h\n";
+	}
+
+	/*void get_in()
 	{
 		driver_inside = true;
 		panel();
@@ -161,40 +205,41 @@ public:
 	void get_out()
 	{
 		driver_inside = false;
-	}
-	void control()
-	{
+	}*/
+	void control() {
 		char key = 0;
-		do
+		do 
 		{
 			key = _getch();
-			switch (key)
+			switch (key) 
 			{
 			case Enter:
-				driver_inside ? get_out() : get_in();
+				if (!driver_inside) 
+				{
+					driver_inside = true;
+					panel_active = true;
+					engine_running = true;
+					thread(&Car::panel_working, this).detach();
+					thread(&Car::engine_working, this).detach();
+				}
+				else 
+				{
+					driver_inside = false;
+					panel_active = false;
+					engine_running = false;
+				}
+				break;
+			case Space:
+				engine_running = !engine_running;
 				break;
 			}
+
 		} while (key != Escape);
 	}
-	void panel()
-	{
-		while (driver_inside)
-		{
-			system("CLS");
-			cout << "Fuel level " << tank.get_fuel_level() << " liters\n";
-			cout << "Engine is " << (engine.started() ? "started" : "stopped") << endl;
-			cout << "Speed:\t" << speed << " km/h\n";
-			Sleep(100);
-		}
-	}
-	void info()const
-	{
-		engine.info();
-		tank.info();
-		cout << "Speed: " << speed << " km/h\n";
-		cout << "MaxSpeed: " << MAX_SPEED << " km/h\n";
-	}
 };
+
+
+
 
 //#define TANK_CHECK
 //#define ENGINE_CHECK
@@ -217,6 +262,6 @@ void main()
 	engine.info();
 #endif // ENGINE_CHECK
 	Car bmw(10, 80, 270);
-	//bmw.info();
 	bmw.control();
+	//bmw.info();
 }
